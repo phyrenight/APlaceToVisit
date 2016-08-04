d5a91a55589388fac6911951d3e0140e470af5aa
Marc <phyrenight@gmail.com>
2016-08-04 16:18:35 -0400

    user game history can now be viewed

--- libs/jsScripts.js
+++ libs/jsScripts.js
@@ -1,86 +1,61 @@
 var map, markers = [], marker;
 function displayPlaces(name){
-	console.log(name);
-	this.name = name;
+	var self = this;
+	console.log(typeof name);
+		self.name =  name;
 }
 
-    $(document).ready(function() {
+$(document).ready(function() {
     function JapanPlaces() {
-        /*this.places = [
-              {
-              	name : "Japan",
-              	address: "Japan",
-              	coords : {lat : "0" ,lng :"0"}
-              },
-              {
-	            name : "Osaka Aquarium Kaiyuan",
-	            address : "1+Chome-1-10+Kaigandori,+Minato+Ward,+Osaka,+Osaka+Prefecture+552-0022,+Japan",
-                coords : {lat :"0", lng : "0"}
-              },
-              {
-	            name : "Akihabara",
-	            address : "Akihabara,+Japan",
-	            coords : { lat : "0", lng : "0"}
-              },
-              {
-	            name : "Nijo Castle",
-	            address : "541+Nijojocho,+Nakagyo+Ward,+Kyoto,+Kyoto+Prefecture+604-8301,+Japan",
-                coords : { lat: "35.0130361", lng : "135.7503697"}
-              },
-              {
-	            name : "Shibuya Crossing", // chase seen in fast and furious tokyo drift before Hanso death
-	            address : "2-29-1+Dogenzaka,+Shibuya-ku,+Tokyo,+Japan",
-	            coords : { lat: "35.6595885", lng: "139.6986289"}
-              },
-              {
-	            name : "KaiKay",
-	            address :"23-7+Maruyamacho,+Shibuya,+Tokyo,+Japan",
-	            coords : {lat: "35.6617773",lng: "139.7040506"}
-              }];*/
-    
-    this.spots = ko.observableArray([
-    	new displayPlaces(places[0]['name']),
-    	new displayPlaces(places[1]['name']),
-    	new displayPlaces(places[2]['name']),
-    	new displayPlaces(places[3]['name']),
-    	new displayPlaces(places[4]['name']),
-    	//new displayPlaces(this.places[5]['name']),
-    	]);
-        }
+    	var self = this;
+        self.places = ko.observableArray([
+        	new displayPlaces(places[0]['names']),
+        	new displayPlaces(places[1]['names']),
+        		new displayPlaces(places[2]['names'])]);
+
+     self.query = ko.observable("");
+    	self.spots = ko.computed(function(){
+    		var search = self.query().toLowerCase();
+    	    return ko.utils.arrayFilter(self.places, function(tempplaces){
+                  return places.toLowerCase().indexOf(search) >= 0;
+    	    });
+    	    	//if(places[i].names.toLowerCase().indexOf(search.toLowerCase()) >= 0){
+    	    	//	self.places.push(new displayPlaces(places[i].names));
+    	    	//}
+    	    		//console.log(self.places.length)
+    	    //}	
+    	    
+    });
+    }
 
 ko.applyBindings(new JapanPlaces());
-
-
-
 });
 places = [
               {
-	            name : "Osaka Aquarium Kaiyuan",
+	            names : "Osaka Aquarium Kaiyuan",
 	            address : "1 Chome-1-10 Kaigandori, Minato Ward, Osaka, Osaka Prefecture 552-0022, Japan",
                 coords : {lat : 34.6550348, lng : 135.4288895}
               },
               {
-	            name : "Akihabara",
+	            names : "Akihabara",
 	            address : "Akihabara, Japan",
 	            coords : { lat : 35.7020691, lng : 139.7753269}
               },
               {
-	            name : "Nijo Castle",
+	            names : "Nijo Castle",
 	            address : "541 Nijojocho, Nakagyo Ward, Kyoto, Kyoto Prefecture 604-8301, Japan",
                 coords : { lat : 35.0130361, lng : 135.7503697}
               },
               {
-	            name : "Shibuya Crossing", // chase seen in fast and furious tokyo drift before Hanso death
+	            names : "Shibuya Crossing", // chase seen in fast and furious tokyo drift before Hanso death
 	            address : "2-29-1 Dogenzaka, Shibuya-ku, Tokyo, Japan",
 	            coords : { lat : 35.6595885, lng : 139.6986289}
               },
               {
-	            name : "KaiKay",
+	            names : "KaiKay",
 	            address :"23-7 Maruyamacho, Shibuya, Tokyo, Japan",
 	            coords : {lat : 35.6617773,lng : 139.7040506}
               }];
-
- //  var address = "Japan"
 
 /*function convertToCoords(address){
 	console.log(address)
@@ -96,53 +71,29 @@ places = [
 }*/
 
 function initMap(){
-	console.log("1st");
       var mapDiv = document.getElementById('map')
       map = new google.maps.Map(mapDiv, {
-
       center: {lat : 36.204824 ,lng : 138.252924},
       zoom: 6
       });
-      makeMapMarker()
-   // makeMapMarker({lat:latt, lng: longi}, map)
-/*
-    var marker = new google.maps.Marker({
-    position: {lat : latt, lng : longi},
-    map:map,
-    title : "Japan"
-    });
-    markers.push(marker);
-    marker.setMap(map)
-*/}
+   //   makeMapMarker()
+}
 
 function makeMapMarker() {
-  //  marker = []
 
-  for(i in places){
-    console.log(places[i].coords)
-    place = places[i];
-    marker = new google.maps.Marker({
-    position: places[i].coords,
-    map:map,
-    title: places[i].name
-
+    for(i in places){
+        marker = new google.maps.Marker({
+        position: places[i].coords,
+        map:map,
+        title: places[i].name
     });
-/*    var contentString = "<div>" + places[i].name + "</div>"+
-                         "<div>"+ places[i].address +"</div>";
-    var infoWindow = new google.maps.InfoWindow({
-    	content : contentString
-    });
-  //  marker.addListener('click', (function(place){
-  //  	infoWindow.open(map,marker);
-  //  })(place));*/
-
     google.maps.event.addListener(marker, 'click', (function(marker, i){
 	    var contentString = "<div>"+ marker.title + "</div>" + 
 	                        "<div>" + places[i].address + "</div>";
 	    var infoWindow = new google.maps.InfoWindow({
             content : contentString,
             closeBoxUrl: ""
-          });
+        });
 	    infoWindow.content = contentString;
 	    return function(){
 	    	infoWindow.open(map, marker);
@@ -151,4 +102,24 @@ function makeMapMarker() {
     markers.push(marker);
   }
     marker.setMap(map)
+
 }
+// apis
+// nytimes api
+nyTimesURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+nyTimesURL += '?' + $.param({"api-key": "b30fd01ee378467091f2076b2edc1c07",
+                             'q': "Japan"});
+        /*$.getJSON(nyTimeUrl, function(data){
+        	var i = 0;
+        	while(i < data['response']['docs'].length){
+        		  content = data['response']['docs'][i];
+// add html element        		  .append("<li class=''><a href='"+ content['web_url']+
+        		  	"'>"+ content['headline']['main']+"</a><p>"
+        		  	+content['snippet']+"</p></li>");
+        		  i += 1;
+        	}
+        })error( function() { 
+// add html element              .append("<h1>Ny times can not load data</h1>")
+        });*/
+
+// yelp api
