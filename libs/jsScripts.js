83a3a3e7759f755a68ed80a308138f73542e71fd
Marc <phyrenight@gmail.com>
2016-09-23 15:38:38 -0400

    added media queries to main heading

--- libs/jsScripts.js
+++ libs/jsScripts.js
@@ -207,11 +207,6 @@ function nonce_generate() {
 function yelpApi(places) {
   var $details = $('#details');
   var yelpHTML = "";
-  var yelp_consumer_key = "XPx515zsSylsRxPZZ2K5tg";
-  var yelp_consumer_secret = "EsQ9fql9IL4XhxOijpP92o7Q92A";
-  var yelp_token = "xN3TgEhTbYzWjk7g7wCz-U7is484UWoy";
-  var yelp_token_secret = "KSQPTAe4BkIe4TXlrS_6Suf6TGk";
-  var yelpURL  = "https://api.yelp.com/v2/search?";
 
   parameters = {
     term: places.names,
---
+++ libs/settings.js
@@ -0,0 +1,5 @@
+  var yelp_consumer_key = "XPx515zsSylsRxPZZ2K5tg";
+  var yelp_consumer_secret = "EsQ9fql9IL4XhxOijpP92o7Q92A";
+  var yelp_token = "xN3TgEhTbYzWjk7g7wCz-U7is484UWoy";
+  var yelp_token_secret = "KSQPTAe4BkIe4TXlrS_6Suf6TGk";
+  var yelpURL  = "https://api.yelp.com/v2/search?";
